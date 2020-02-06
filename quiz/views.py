7eaf931d3198c845f583db7ef9ee86af1e0c955c
Arvish_Doshi <arvish.j@simformsolutions.com>
2020-02-06 16:53:02 +0530

    Initial Commit

---
+++ quiz/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    return render(request, "home.html")
+
